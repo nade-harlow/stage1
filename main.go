bd795a88d7b4affa366737e94f00be9729bb7a79
nade-harlow <omonadefranklyn@gmail.com>
2022-10-28 09:48:08 +0100

    stage 1

--- main.go
+++ main.go
@@ -12,9 +12,26 @@ type slackUser struct {
 	Bio           string `json:"bio"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
 	user := newUser()
+	r.Use(CORSMiddleware())
 	r.GET("/", user.GetSlackUser())
 	err := r.Run()
 	if err != nil {
